73649e25ce564f5edc35d708c10a134abaf309ec
marekspojda <marek.spojda@o2.pl>
2018-12-10 11:54:54 +0100

    Solving minor problems with repo

--- src/KostkaDoGry.java
+++ src/KostkaDoGry.java
@@ -1,5 +1,6 @@
 public class KostkaDoGry {
     public static void main(String[] args) {
         System.out.println("Róbmy kostkę do gry!");
+        System.out.println("Hope we can make it!");
     }
 }
